f60be90acb6a8223c498038bdcb39aa3fc78dbc8
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-02-19 20:58:29 -0500

    seccion datatable casi terminada

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -14,6 +14,7 @@ namespace Vidly.Dtos
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public byte MembershipTypeId { get; set; }
+        public MembershipTypeDto MembershipType { get; set; }
         [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
